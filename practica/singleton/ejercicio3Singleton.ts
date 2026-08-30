27bc16c281d52cebddb4242beccc07711a35a58d
User <user@example.com>
2026-08-30 12:38:30 -0300

    se termino el ejercicio 3 de singleton

--- practica/singleton/ejercicio3Singleton.ts
+++ practica/singleton/ejercicio3Singleton.ts
@@ -1,3 +1,39 @@
+class SesionUsuario {
+    private nombreUsuario: string = "";
+    private rol: "admin" | "usuario" | "invitado" = "invitado";
+    private iniciada: boolean = false;
+    private static instancia: SesionUsuario;
+
+    private constructor(){}
+
+    static obtenerInstancia(): SesionUsuario {
+        SesionUsuario.instancia ??= new SesionUsuario()
+        return SesionUsuario.instancia
+    }
+    iniciarSesion(nombre: string, rol:"admin" | "usuario" | "invitado"){
+        this.nombreUsuario = nombre;
+        this.rol = rol
+        this.iniciada = true;
+    }
+
+    cerrarSesion(){
+        this.iniciada = false;
+        this.nombreUsuario = "";  
+    }
+
+    obtenerInformacion(){
+        return { nombreUsuario: this.nombreUsuario, rol: this.rol, iniciada: this.iniciada };
+    }
+}
+
+
+
+
+
+
+
+
+
 // Crea una clase SesionUsuario que siga el patrón Singleton.
 
 // Debe tener propiedades privadas para:
